0641abeb68131dd63ed8c3529e229dd6b982395a
FadyGamilM <gamilfady605@gmail.com>
2024-09-27 02:07:31 +0300

    INFRASTRUCTURE | apply the dynamodb table into localstack cloudFormation stack

--- infra/main.tf
+++ infra/main.tf
@@ -1,12 +1,12 @@
-variable "access_key" {
+variable "aws_access_key" {
     type = string 
 }
 
-variable "secret_access_key" {
+variable "aws_secret_access_key" {
     type = string
 }
 
-variable "region" {
+variable "aws_region" {
     type = string
 }
 
@@ -20,9 +20,9 @@ variable "dynamodb_table_name" {
 
 
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_access_key
-  region = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_access_key
+  region = var.aws_region
 
   skip_credentials_validation = true
   skip_metadata_api_check = true
@@ -30,7 +30,7 @@ provider "aws" {
 
 
   endpoints {
-     sqs = var.localstack_endpoint
+    dynamodb = var.localstack_endpoint
   }
 }
 
@@ -39,6 +39,8 @@ resource "aws_dynamodb_table" "test_table" {
   name           = var.dynamodb_table_name
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
+    range_key      = "createdAt"  # Using createdAt as the sort key
+
 
   attribute {
     name = "id"
@@ -50,18 +52,19 @@ resource "aws_dynamodb_table" "test_table" {
     type = "S" # aws.String(time.Now().Format(time.RFC3339))
   }
 
-    attribute {
-      name = "content"
-      type = "S"
-    }
+#! Why commented ? becasue in terraform we don't have to define all attributed, we just define the hash key and the sort key only 
+  # attribute {
+  #   name = "content"
+  #   type = "S"
+  # }
 
-    attribute {
-      name = "password"
-      type = "S"
-    }
+  # attribute {
+  #   name = "password"
+  #   type = "S"
+  # }
 
-    attribute {
-      name = "url"
-      type = "S"
-    }
+  # attribute {
+  #   name = "url"
+  #   type = "S"
+  # }
 }
